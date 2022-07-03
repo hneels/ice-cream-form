afb79ee8f5a9c8d7b22ee5476122aa623c316c6b
hneels <hope.neels@gmail.com>
2022-07-03 10:09:10 -0400

    first commit

---
+++ readme.md
@@ -0,0 +1,14 @@
+# Ice Cream Form
+
+*This project was an assignment for BU MET CS601 and copying any portion of it is a violation of Boston University's Academic Conduct Code.*
+
+<img src="screenshot.png" alt="screenshot of Ice Cream Form" width="600px">
+
+## Technologies
+* HTML
+* CSS
+* JavaScript
+* Regular Expressions
+
+## Summary
+This project focuses on pretty front-end design and JavaScript event listeners. There are two kinds of form validation: the submit button is only enabled if all fields are completed, and once the form is submitted the data is verified and an error is displayed if the facilitator name is not one of the four pre-selected options or the first/last name are not properly formed.
---
+++ scripts.js
@@ -0,0 +1,96 @@
+/* 
+CS601 Assignment 4
+Author: Hope Asher
+*/
+
+// HTML elements repeatedly used within functions below
+const submitButton = document.querySelector("#submit");
+const firstName = document.querySelector("#firstName");
+const lastName = document.querySelector("#lastName");
+const facilitator = document.querySelector("#facilitator");
+const warning = document.querySelector("#warning");
+
+// disable submit button until input validated
+submitButton.disabled = true;
+
+// check for valid first name to enable submit button
+firstName.addEventListener("keyup", enableButton);
+
+// check for valid last name to enable submit button
+lastName.addEventListener("keyup", enableButton);
+
+// check for valid facilitator after field loses focus
+facilitator.addEventListener("blur", validFacil);
+
+// clear warning box when user goes back to facilitator input to try again
+facilitator.addEventListener("focus", () => {
+    warning.innerHTML = "";
+});
+
+
+// validate all inputs before submitting form
+// assignment note: all fields are re-checked here in case user bypasses the submit button or ignores warnings
+document.querySelector("form").addEventListener("submit", (event) => {
+
+    // check if all three inputs are valid, using helper functions below
+    if (!(validFacil() && validName(firstName) && validName(lastName))) {
+
+        // if not valid, show warning alert and prevent form submission
+        warning.innerHTML = "&#9888; Invalid input";
+        event.preventDefault();
+    }
+});
+
+
+// helper function to only enable submit button if first and last name are valid
+function enableButton() {
+
+    // testing both conditions before the if-statement so they both evaluate
+    // otherwise the lastName condition is short-circuited and className not properly changed sometimes
+    const firstValid = validName(firstName);
+    const lastValid = validName(lastName);
+
+    // enable the submit button if both name inputs are valid
+    if (firstValid && lastValid) {
+        submitButton.disabled = false;
+    }
+    // re-disable it (in case user deletes input)
+    else {
+        submitButton.disabled = true;
+    }
+}
+
+// helper function to check length and characters of a name input field
+function validName(name) {
+
+    // match regex: two or more instances of only a-z or A-Z (no spaces)
+    const regex = /^[a-zA-Z]{2,}$/;
+    const validMatch = regex.test(name.value);
+
+    // return true only if length and characters are proper
+    if (validMatch) {
+        name.className = "";
+        return true;
+    } else {
+        // change input class to show invalid input
+        name.className = "invalid";
+        return false;
+    }
+}
+
+// helper function to check if facilitator is in list of approved facilitators
+function validFacil() {
+    const facilOptions = ["Chris", "Christian", "Josh", "Fazil"];
+
+    // if the value typed in facilitator field matches a name in the facilitator array
+    if (facilOptions.includes(facilitator.value)) {
+        facilitator.className = "";
+        return true;
+    } else {
+        // display warning icon and message below facilitator input
+        warning.innerHTML = "&#9888; invalid facilitator";
+        // highlight invalid input with colored border
+        facilitator.className = "invalid";
+        return false;
+    }
+}
